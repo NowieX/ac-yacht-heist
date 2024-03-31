ccc09c519de060fa69fe5ca1b9cbe43788b38f5b
Noah Lentfert <noah00215@gmail.com>
2024-03-31 16:26:32 +0200

    Made code better

--- client/client.lua
+++ client/client.lua
@@ -36,39 +36,65 @@ function CreatePoliceReport(player, message, message_head)
     })
 end
 
-CreateThread(function()
-    for _, value in pairs(Config.HeistNPC) do
-        RequestModel(GetHashKey(value.model))
-        while not HasModelLoaded(GetHashKey(value.model)) do
-            Wait(1)
+local function PlayerIsInRange(player, coords)
+    while true do
+        Citizen.Wait(1500)
+        local player_coords = GetEntityCoords(player)
+        local distance = #(coords - player_coords)
+
+        if distance < 200 then
+            return true
         end
-
-        npc = CreatePed(2, value.model, value.location.x, value.location.y, value.location.z - 1, value.location.w,  false, true)
-
-        SetPedFleeAttributes(npc, 0, 0)
-        SetPedDropsWeaponsWhenDead(npc, false)
-        SetPedDiesWhenInjured(npc, false)
-        SetEntityInvincible(npc , true)
-        FreezeEntityPosition(npc, true)
-        SetBlockingOfNonTemporaryEvents(npc, true)
-
-        TaskStartScenarioInPlace(npc, 'WORLD_HUMAN_GUARD_STAND_FACILITY', 0, true)
-
-        exports.ox_target:addBoxZone({
-            coords = vec3(value.location.x, value.location.y, value.location.z),
-            size = vec3(1, 1, 1),
-            rotation = 360,
-            debug = Config.Debugger,
-            options = {
-                {
-                    serverEvent = 'ac-yacht-heist:server:PassAllChecks',
-                    distance = Config.GeneralTargetDistance,
-                    icon = 'fa fa-ship',
-                    label = Config.Translations['BossInformation'].target_label,
-                },
-            }
-        })
     end
+end
+
+local function TeleportPlayerToLocation(player_coords, player_heading)
+    local playerPed = GetPlayerPed(-1)
+    DoScreenFadeOut(Config.HeistInformation['BlackScreenTimer'])
+    Citizen.Wait(Config.HeistInformation['BlackScreenTimer'])
+    SetEntityHeading(playerPed, player_heading)
+    SetEntityCoords(playerPed, player_coords.x, player_coords.y, player_coords.z, true, false, false, false)
+    Citizen.Wait(Config.HeistInformation['BlackScreenTimer'])
+    DoScreenFadeIn(Config.HeistInformation['BlackScreenTimer'])
+end
+
+CreateThread(function()
+    AddDoorToSystem(`door_camera_building`, `v_ilev_rc_door2`, 1005.2922363281, -2998.2661132812, -47.496891021729, false, false, false)
+    DoorSystemSetDoorState(`door_camera_building`, 1, false, true)
+
+    local heist_npc = Config.HeistNPC
+
+    RequestModel(GetHashKey(heist_npc.model))
+    while not HasModelLoaded(GetHashKey(heist_npc.model)) do
+        Wait(1)
+    end
+
+    npc = CreatePed(2, heist_npc.model, heist_npc.location.x, heist_npc.location.y, heist_npc.location.z - 1, heist_npc.heading,  false, true)
+
+    SetPedFleeAttributes(npc, 0, 0)
+    SetPedDropsWeaponsWhenDead(npc, false)
+    SetPedDiesWhenInjured(npc, false)
+    SetEntityInvincible(npc , true)
+    FreezeEntityPosition(npc, true)
+    SetBlockingOfNonTemporaryEvents(npc, true)
+
+    TaskStartScenarioInPlace(npc, 'WORLD_HUMAN_GUARD_STAND_FACILITY', 0, true)
+
+    exports.ox_target:addBoxZone({
+        coords = vec3(heist_npc.location.x, heist_npc.location.y, heist_npc.location.z),
+        size = vec3(1, 1, 1),
+        rotation = 360,
+        debug = Config.Debugger,
+        options = {
+            {
+                serverEvent = 'ac-yacht-heist:server:PassAllChecks',
+                distance = Config.GeneralTargetDistance,
+                icon = 'fa fa-ship',
+                label = Config.HeistNPC.target_label,
+            },
+        }
+    })
+    
 end)
 
 RegisterNetEvent('ac-yacht-heist:client:OpenMenuHeist', function ()
@@ -85,20 +111,19 @@ end)
 RegisterNUICallback("ac-yacht-heist:client:StartHackingPreperation", function ()
     TriggerServerEvent("ac-yacht-heist:server:HeistStarted")
     SetNuiFocus(false,false)
-    local playerPed = PlayerPedId()
 
     lib.notify({
-        title = Config.Translations["BossInformation"].boss_title,
-        description = Config.Translations['BossInformation'].introduction_round.message,
-        duration = Config.Translations['BossInformation'].introduction_round.timer, 
-        position = Config.Notifies.position, 
+        title = Config.HeistNPC.boss_title,
+        description = Config.HeistNPC.introduction_round.message,
+        duration = Config.HeistNPC.introduction_round.timer, 
+        position = Config.Notifies.position,
         type = 'info'
     })
 
-    local hack_building_entrance_blip = CreateBlip(Config.HeistLocations.HackerBuilding.location.x, Config.HeistLocations.HackerBuilding.location.y, Config.HeistLocations.HackerBuilding.location.z, 472, 1.0, 59, true, "Camera's hacken")
+    local hack_building_entrance_blip = CreateBlip(Config.HeistLocations.HackerBuilding.entrance.coords.x, Config.HeistLocations.HackerBuilding.entrance.coords.y, Config.HeistLocations.HackerBuilding.entrance.coords.z, 472, 1.0, 59, true, "Camera's hacken")
     
     Camera_Building_Zone = exports.ox_target:addBoxZone({
-        coords = vec3(Config.HeistLocations.HackerBuilding.location.x, Config.HeistLocations.HackerBuilding.location.y, Config.HeistLocations.HackerBuilding.location.z),
+        coords = vec3(Config.HeistLocations.HackerBuilding.entrance.coords.x, Config.HeistLocations.HackerBuilding.entrance.coords.y, Config.HeistLocations.HackerBuilding.entrance.coords.z),
         size = vec3(1, 1, 1),
         rotation = 360,
         debug = Config.Debugger,
@@ -108,8 +133,8 @@ RegisterNUICallback("ac-yacht-heist:client:StartHackingPreperation", function ()
                     TriggerEvent('ac-yacht-heist:client:EnterCameraBuilding', hack_building_entrance_blip)
                 end,
                 distance = Config.GeneralTargetDistance,
-                icon = 'fa fa-network-wired',
-                label = Config.HeistLocations.HackerBuilding.target_label,
+                icon = 'fa fa-door-open',
+                label = Config.HeistLocations.HackerBuilding.entrance.target_label,
             },
         }
     })
@@ -119,17 +144,13 @@ RegisterNetEvent("ac-yacht-heist:client:EnterCameraBuilding", function (entrance
     RemoveBlip(entrance_blip)
     local security_panel_coords = Config.HeistLocations.Security_panel_hack_scene.scene_location
     exports.ox_target:removeZone(Camera_Building_Zone)
-    DoScreenFadeOut(Config.HeistInformation['BlackScreenTimer'])
-    Citizen.Wait(Config.HeistInformation['BlackScreenTimer'])
-    local playerPed = GetPlayerPed(-1)
-    SetEntityHeading(playerPed, 82.7456)
-    SetEntityCoords(playerPed, 1004.1747, -2997.6816, -47.6471, true, false, false, false)
+
+    TeleportPlayerToLocation(vec3(1004.1747, -2997.6816, -47.6471), 82.7456)
+
     local security_panel = CreateObject(`hei_prop_hei_securitypanel`, security_panel_coords.x, security_panel_coords.y, security_panel_coords.z, true, true, false)
     SetEntityHeading(security_panel, Config.HeistLocations.Security_panel_hack_scene.scene_rotation.z)
-    Citizen.Wait(Config.HeistInformation['BlackScreenTimer'])
-    DoScreenFadeIn(Config.HeistInformation['BlackScreenTimer'])
 
-    Camera_Building_Zone = exports.ox_target:addBoxZone({
+    Hacking_Zone = exports.ox_target:addBoxZone({
         coords = vec3(security_panel_coords.x, security_panel_coords.y, security_panel_coords.z),
         size = vec3(1, 1, 1),
         rotation = 360,
@@ -140,6 +161,35 @@ RegisterNetEvent("ac-yacht-heist:client:EnterCameraBuilding", function (entrance
                 distance = Config.GeneralTargetDistance,
                 icon = 'fa fa-camera',
                 label = Config.HeistLocations.Security_panel_hack_scene.target_label,
+            },
+        }
+    })
+end)
+
+RegisterNetEvent("ac-yacht-heist:client:ExitCameraBuilding", function ()
+
+    lib.notify({
+        title = Config.HeistNPC.boss_title,
+        description = Config.HeistLocations.Security_panel_hack_scene.notification.label,
+        duration = Config.HeistLocations.Security_panel_hack_scene.notification.timer, 
+        position = Config.Notifies.position,
+        type = 'info'
+    })
+
+    Hacking_Zone = exports.ox_target:addBoxZone({
+        coords = vec3(Config.HeistLocations.HackerBuilding.exit.coords.x, Config.HeistLocations.HackerBuilding.exit.coords.y, Config.HeistLocations.HackerBuilding.exit.coords.z),
+        size = vec3(1, 1, 1),
+        rotation = 360,
+        debug = Config.Debugger,
+        options = {
+            {
+                onSelect = function ()
+                    TeleportPlayerToLocation(vec3(-297.6677, 6391.5552, 30.6124), 123.6319)
+                    TriggerServerEvent("ac-yacht-heist:server:SpawnBoatToSteal")
+                end,
+                distance = Config.GeneralTargetDistance,
+                icon = 'fa fa-door-open',
+                label = Config.HeistLocations.HackerBuilding.exit.target_label,
             },
         }
     })
